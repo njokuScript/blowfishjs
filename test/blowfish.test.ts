89ff745d236f54518895b5a9f0915c522c4fcfae
Njoku Emmanuel <kalunjoku123@gmail.com>
2022-12-22 08:35:21 +0100

    Chore: setup jest and run test

--- test/blowfish.test.ts
+++ test/blowfish.test.ts
@@ -1,5 +1,9 @@
 import Blowfish from '../src/index'
 
 describe('Blowfish Init', () => {
-  it('should return blowfish value')
+  it('should return blowfish value', () => {
+    const value = Blowfish('Welcome to Blowfish')
+    console.log(value)
+    expect(value).toBe('Welcome to Blowfish')
+  })
 })
